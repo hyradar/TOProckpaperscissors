1777bbef893e8f99481d4cd51571fa47e43a162e
Hunter Yeago <hyradar@tutamail.com>
2022-08-25 11:13:13 +1000

    made random computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+    let rand = Math.floor((Math.random()) * 3) + 1;
+    let computerchoice;
+
+    if (rand == 1) {
+        computerChoice = "rock";
+    }
+    else if (rand == 2) {
+        computerChoice = "paper";
+    }
+    else if (rand == 3) {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
+
+//Start the game
+
+
+let computerChoice;
+computerChoice = getComputerChoice();
+console.log(computerChoice);
